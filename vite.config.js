fe93da2e4860bbfd4da6b60b86c6dea328d2a787
chidy <chidy@yusys.com.cn>
2022-12-13 17:07:02 +0800

    static

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/.',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -12,7 +13,7 @@ export default defineConfig({
     }
   },
   build : {
-  	outDir : 'doc',
+  	outDir : 'dist',
     assetsDir: 'assets',
   },
 })
